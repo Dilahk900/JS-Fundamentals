b9fab7ad34e3ac3f1450e0e04bd1b9f43ffb4cbb
Dilahk900 <khalid.idrissif@gmail.com>
2025-05-23 22:50:04 +0200

    2-arguments.js added

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+let number_of_argument = 0;
+
+if(number_of_argument == 1){
+    console.log('Argument found');
+} else
+if(number_of_argument > 1){
+    console.log('Arguments found');
+} else console.log('No argument');
