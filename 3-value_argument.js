bfa86ed6eabb138faebcab815903c5f988feb91c
Dilahk900 <khalid.idrissif@gmail.com>
2025-05-24 01:03:58 +0200

    3-value_argument.js added

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,11 +1,11 @@
 let number_of_argument = process.argv.slice(2);
 let i = 0;
 
-if(!number_of_argument[0]){
-    console.log('No arguments');
-}else
-while(number_of_argument[i]){
+if(number_of_argument[0]){
+    while(number_of_argument[i]){
     console.log(number_of_argument[i]);
     i ++;
-} 
+    } 
+}else console.log('No arguments');
+
 
