182f348a9d3c7c4ee56d8d5a9b2e5fc8ee50e40e
Dilahk900 <khalid.idrissif@gmail.com>
2025-05-24 14:37:49 +0200

    6-multi_languages_loop.js added

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -4,6 +4,6 @@ languages[0] = 'C is fun';
 languages[1] = 'Python is cool';
 languages[2] = 'JavaScript is amazing';
 
-for(let i = 0; i <= 2; i++){
+for (let i = 0; i <= 2; i++){
     console.log(languages[i]);
 }
