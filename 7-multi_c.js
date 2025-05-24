deb083f5ec6f9e0710caf5a871d6e537974869cb
Dilahk900 <khalid.idrissif@gmail.com>
2025-05-24 15:53:50 +0200

    7-multi_c.js added

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,6 +2,6 @@ let num = process.argv.slice(2);
 
 if(!num[0]){
     console.log('Missing number of occurrences');
-}else for(let i = 0; i < num[0]; i++){
+}else for (let i = 0; i < num[0]; i++){
         console.log('C is fun');
         }
